3211bb275dc112aeecd0f2a29a3266143ecb4fe0
JieKun <1203664512@qq.com>
2019-06-18 16:53:19 +0800

    Read

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@
 		$("#btn").click(function() {
 			layer.open({
 				type: 2,
-				title: " ",
+				title: '易烊千玺<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>',
 				content: "new_file.html",
 				area: ['500px', '320px'],
 				cancel: function(index, layero) {
--- js/new_file.js
+++ js/new_file.js
@@ -1,12 +1,14 @@
 (function() {
 	var _layer;
 	var nums = 0;
+	var numss
 
 	layui.use(['layer'], function() {
 		_layer = layui.layer;
 
 		$("#no").click(function() {
 			if (nums == 0) {
+				numss = 0;
 				nums++;
 				layer.alert('一起看到电影的结尾', {
 					title: " ",
@@ -20,7 +22,8 @@
 				});
 			} else if (nums == 2) {
 				nums = 0;
-				layer.alert('2019.6.7', {
+				numss = 2;
+				layer.alert('2019.6.27', {
 					title: " ",
 					closeBtn:0
 				});
@@ -28,11 +31,18 @@
 		});
 
 		$("#yes").click(function() {
-			layer.alert('<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>' +
-				'<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>' +
-				'<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>', {
-					title: " "
-				});
+			// if(numss == 2){
+				layer.alert('<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>' +
+					'<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>' +
+					'<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>', {
+						title: " "
+					});
+// 			}else{
+// 				layer.alert('剧情不是这样的！！！', {
+// 					title: " ",
+// 					closeBtn:0
+// 				});
+// 			}
 		});
 	})
 })();
