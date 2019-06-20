f6be67601693c409a472be0ce822addbb0226aa1
JieKun <1203664512@qq.com>
2019-06-20 17:46:44 +0800

    _

--- js/index.js
+++ js/index.js
@@ -1,22 +1,33 @@
 (function() {
+	var _index;
 
-
-
-	layui.use(['layer'], function() {
+	layui.use(['layer','carousel'], function() {
 		layer = layui.layer;
+		var carousel = layui.carousel;
+		//建造实例
+		carousel.render({
+			elem: '#test1',
+			width: '401px' ,//设置容器宽度
+			height:'351px',
+			interval:4000,
+			arrow: 'none', //始终显示箭头
+			anim: 'fade' //切换动画方式
+		});
 
 		$("#btn").click(function() {
 			layer.open({
 				type: 2,
 				title: '易烊千玺<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>',
 				content: "new_file.html",
-				area: ['500px', '320px'],
+				area: ['500px', '330px'],
 				cancel: function(index, layero) {
+					_index = index;
 					layer.alert("我们不见不散");
-					
 					return false;
 				}
-			})
+			});
+			sessionStorage.setItem("index", _index);
+			console.log(layer.index);
 		});
 
 	});
--- js/new_file.js
+++ js/new_file.js
@@ -1,48 +1,45 @@
 (function() {
-	var _layer;
+	var layer;
 	var nums = 0;
-	var numss
 
 	layui.use(['layer'], function() {
-		_layer = layui.layer;
+		layer = layui.layer;
 
 		$("#no").click(function() {
 			if (nums == 0) {
-				numss = 0;
 				nums++;
 				layer.alert('一起看到电影的结尾', {
 					title: " ",
-					closeBtn:0
+					closeBtn: 0
 				});
 			} else if (nums == 1) {
 				nums++;
 				layer.alert('好吗', {
 					title: " ",
-					closeBtn:0
+					closeBtn: 0
 				});
 			} else if (nums == 2) {
 				nums = 0;
-				numss = 2;
 				layer.alert('2019.6.27', {
 					title: " ",
-					closeBtn:0
+					closeBtn: 0
 				});
 			}
 		});
 
 		$("#yes").click(function() {
-			// if(numss == 2){
-				layer.alert('<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>' +
-					'<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>' +
-					'<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>', {
-						title: " "
-					});
-// 			}else{
-// 				layer.alert('剧情不是这样的！！！', {
-// 					title: " ",
-// 					closeBtn:0
-// 				});
-// 			}
+			layer.confirm('<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>' +
+				'<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>' +
+				'<svg class="icon" aria-hidden="true"><use xlink:href="#icon-xinxing"></use></svg>', 
+				{
+					btn:['确定'],
+					title: " ",
+					closeBtn:0
+				},function(){
+					layer.closeAll(); 
+					parent.layer.close(parent.layer.getFrameIndex(window.name));
+				}
+			);
 		});
 	})
 })();
